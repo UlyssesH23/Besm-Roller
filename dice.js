a33de86c8cc4957d5a4f8102cad2c05803799ebe
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2021-06-10 12:45:37 -0700

    Fixed global variables
    Made all global variables part of a single global project.

--- dice.js
+++ dice.js
@@ -5,25 +5,50 @@ const bodyParser = require('body-parser');
 const https = require("https");
 const app = express();
 app.use(express.static("public"));
-var characterName;
-var bodyStat;
-var mindStat;
-var soulStat;
-var dcv;
-var acv;
-var dieOneNumber;
-var dieTwoNumber;
-var diceTotal;
-var typeOfRoll = "Roll";
+
+var MYAPP = {};
+
+MYAPP.character = {
+    name: "Character",
+    bodyStat: 0,
+    mindStat: 0,
+    soulStat: 0,
+    dcv: 0,
+    acv: 0,
+
+
+}
+
+MYAPP.dice = {
+    dieOneNumber: 0,
+    dieTwoNumber: 0,
+    typeOfRoll: "Roll",
+}
+
+MYAPP.results = {
+    diceTotal: 0,
+    totalOutput: 0,
+}
+
+// var characterName;
+// //var bodyStat;
+// var mindStat;
+// var soulStat;
+// var dcv;
+// var acv;
+//var dieOneNumber;
+// var dieTwoNumber;
+// var diceTotal;
+// var typeOfRoll = "Roll";
 app.set('view engine', 'ejs');
 
-var characterInfo = {
-    dieOneNumber: dieOneNumber,
-    dieTwoNumber: dieTwoNumber,
-    diceTotal: diceTotal,
-    typeOfRoll: typeOfRoll,
-    characterName: characterName
-}
+// var characterInfo = {
+//     dieOneNumber: dieOneNumber,
+//     dieTwoNumber: dieTwoNumber,
+//     diceTotal: diceTotal,
+//     typeOfRoll: typeOfRoll,
+//     characterName: characterName
+// }
 
 // variables/funtctions used throughout
 
@@ -34,10 +59,10 @@ function getRandomInt(max) {
 }
 
 function rollDice() {
-    dieOneNumber = getRandomInt(6) + 1;
-    dieTwoNumber = getRandomInt(6) + 1;
+    MYAPP.dice.dieOneNumber = getRandomInt(6) + 1;
+    MYAPP.dice.dieTwoNumber = getRandomInt(6) + 1;
 
-    diceTotal = dieOneNumber + dieTwoNumber;
+    MYAPP.results.diceTotal = MYAPP.dice.dieOneNumber + MYAPP.dice.dieTwoNumber;
 }
 
 
@@ -55,121 +80,111 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-    characterName = req.body.cName;
-    mindStat = parseInt(req.body.mStat);
-    bodyStat = parseInt(req.body.bStat);
-    soulStat = parseInt(req.body.sStat);
-    dcv = parseInt(req.body.dcv);
-    acv = parseInt(req.body.acv);
+    MYAPP.character.name = req.body.cName;
+    MYAPP.character.mindStat = parseInt(req.body.mStat);
+    MYAPP.character.bodyStat = parseInt(req.body.bStat);
+    MYAPP.character.soulStat = parseInt(req.body.sStat);
+    MYAPP.character.dcv = parseInt(req.body.dcv);
+    MYAPP.character.acv = parseInt(req.body.acv);
     rollDice();
 
+    console.log(MYAPP);
+
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.diceTotal,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
 });
 app.post('/editStats', function(req, res) {
-    res.sendFile(__dirname + "/info.html")
+    res.render('editStats', {
+
+    })
 });
 
 app.post('/regRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
+    MYAPP.dice.typeOfRoll = "Roll";
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.diceTotal,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
+
 });
 
 app.post('/mindRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "Mind Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
-    diceTotal = diceTotal + mindStat;
+    MYAPP.dice.typeOfRoll = "Mind Roll";
+    MYAPP.results.totalOutput = MYAPP.results.diceTotal + MYAPP.character.mindStat;
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.totalOutput,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
+
 
 });
 
 
 app.post('/bodyRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "Body Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
-    diceTotal = diceTotal + bodyStat;
+    MYAPP.dice.typeOfRoll = "Body Roll";
+    MYAPP.results.totalOutput = MYAPP.results.diceTotal + MYAPP.character.bodyStat;
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.totalOutput,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
+
 });
 
 app.post('/soulRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "Soul Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
-    diceTotal = diceTotal + soulStat;
+    MYAPP.dice.typeOfRoll = "Soul Roll";
+    diceTotal = MYAPP.results.diceTotal + MYAPP.character.soulStat;
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.totalOutput,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
+
 });
 
 app.post('/acvRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "ACV Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
-    diceTotal = diceTotal + acv;
+    MYAPP.dice.typeOfRoll = "ACV Roll";
+    MYAPP.results.totalOutput = MYAPP.results.diceTotal + MYAPP.character.acv;
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.totalOutput,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
 });
 
 app.post('/dcvRoll', function(req, res) {
     rollDice();
-    typeOfRoll = "DCV Roll";
-    console.log(dieOneNumber);
-    console.log(dieTwoNumber);
-    diceTotal = diceTotal + dcv;
+    MYAPP.dice.typeOfRoll = "DCV Roll";
+    MYAPP.results.totalOutput = MYAPP.results.diceTotal + MYAPP.character.dcv;
     res.render('dice', {
-        dieOneNumber: dieOneNumber,
-        dieTwoNumber: dieTwoNumber,
-        diceTotal: diceTotal,
-        typeOfRoll: typeOfRoll,
-        characterName: characterName
+        dieOneNumber: MYAPP.dice.dieOneNumber,
+        dieTwoNumber: MYAPP.dice.dieTwoNumber,
+        totalOutput: MYAPP.results.totalOutput,
+        typeOfRoll: MYAPP.dice.typeOfRoll,
+        characterName: MYAPP.character.name,
     });
-    console.log(diceTotal);
 });
 
 //
